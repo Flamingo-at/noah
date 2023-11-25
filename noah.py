983a3bac90086295771d1655cb5623d5a2bc74b1
Flamingo-at <119711235+Flamingo-at@users.noreply.github.com>
2023-11-25 15:28:53 +0200

    Add files via upload

--- noah.py
+++ noah.py
@@ -3,112 +3,113 @@ import requests
 
 from re import findall
 from loguru import logger
+from random import choice
+from string import ascii_letters
 from concurrent.futures import ThreadPoolExecutor
 from pyuseragents import random as random_useragent
+from tenacity import retry, retry_if_exception, stop_after_attempt, wait_fixed
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 
 
 def create_email():
     try:
         response = requests.get('https://www.1secmail.com/api/v1/?action=genRandomMailbox&count=1')
         email = response.json()[0]
+        if email.split('@')[1] not in ['laafd.com', 'vjuum.com', 'txcct.com']:
+            return create_email()
         return email
     except Exception:
         logger.error('Failed to create email')
-        time.sleep(1)
-        return(create_email())
+        time.sleep(5)
+        return create_email()
 
 
+@retry(retry=retry_if_exception(Exception), stop=stop_after_attempt(60), wait=wait_fixed(1), reraise=True)
 def check_email(login: str, domain: str, count: int):
     try:
         response = requests.get('https://www.1secmail.com/api/v1/?action=getMessages&'
                                 f'login={login}&domain={domain}')
         email_id = response.json()[0]['id']
-        return(email_id)
+        return email_id
     except:
-        while count < 30:
-            count += 1
-            time.sleep(1)
-            return(check_email(login, domain, count))
-        logger.error('Emails not found')
-        raise Exception()
+        raise Exception('Emails not found')
 
 
-def get_link(login: str, domain: str, email_id):
+def get_code(login: str, domain: str, email_id: int):
     try:
         response = requests.get('https://www.1secmail.com/api/v1/?action=readMessage&'
                                 f'login={login}&domain={domain}&id={email_id}')
-        data = response.json()['body']
-        link = findall(
-            r'"(https:\/\/auth.noah.com\/u\/email-verification.+)"', data)[0]
-        return(link)
+        data = response.json()['htmlBody']
+        link = findall(r'code=(\d{6})', data)[0]
+        return link
     except:
-        logger.error('Failed to get link')
-        raise Exception()
+        raise Exception('Failed to get link')
 
 
-def main(ref):
+def main():
     while True:
         try:
             logger.info('Get email')
             email = create_email()
 
             options = webdriver.ChromeOptions()
-            options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+            options.add_experimental_option('excludeSwitches', ['enable-automation', 'enable-logging'])
             options.add_argument('--disable-blink-features=AutomationControlled')
             options.add_argument(f'user-agent={random_useragent()}')
             options.add_argument('--disable-infobars')
             options.add_argument('--disable-gpu')
             options.add_argument('--mute-audio')
-            options.headless = True
+            options.add_argument('--headless')
 
             driver = webdriver.Chrome(options=options)
-            actions = ActionChains(driver)
-            wait = WebDriverWait(driver, 15)
-
-            driver.get(f"https://app.noah.com/?referralCode={ref}")
+            wait = WebDriverWait(driver, 30)
+            driver.get(f'https://app.noah.com/?referralCode={REF_CODE}')
 
             logger.info('Create an account')
-            wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Create an account')]"))).click()
+            wait.until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), 'Sign up')]'))).click()
             wait.until(EC.element_to_be_clickable((By.ID, 'email'))).send_keys(email)
-            wait.until(EC.element_to_be_clickable((By.ID, 'password'))).send_keys(email)
-            wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Create Account')]"))).click()
+            password = ''.join(choice(ascii_letters) for _ in range(10))
+            wait.until(EC.element_to_be_clickable((By.ID, 'password'))).send_keys(password)
+            wait.until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), 'Sign up')]'))).click()
 
             logger.info('Check email')
             email_id = check_email(email.split('@')[0], email.split('@')[1], 0)
 
-            logger.info('Get link')
-            link = get_link(email.split('@')[0], email.split('@')[1], email_id)
+            logger.info('Get code')
+            verification_сode = get_code(email.split('@')[0], email.split('@')[1], email_id)
 
             logger.info('Confirm email')
-            driver.get(link)
+            wait.until(EC.element_to_be_clickable((By.ID, 'verificationCode'))).send_keys(verification_сode)
+            wait.until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), 'Continue')]'))).click()
 
-            wait.until(EC.element_to_be_clickable((By.XPATH, "//a[contains(text(), 'Continue')]"))).click()
-            time.sleep(5)
-            wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Next')]"))).click()
+            logger.info('Set nickname')
+            wait.until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), 'Next')]'))).click()
+            wait.until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), 'Continue')]'))).click()
+            wait.until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), 'Not now')]'))).click()
             time.sleep(2)
-            wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Save & Enter NOAH')]"))).click()
 
-        except:
-            logger.error('Error')
+        except Exception as error:
+            logger.error(error)
 
         else:
             with open('registered.txt', 'a', encoding='utf-8') as f:
-                f.write(f'{email}\n')
+                f.write(f'{email}:{password}\n')
             logger.success('Successfully\n')
+
+        finally:
+            driver.quit()
 
 
 if __name__ == '__main__':
     print('Bot Noah @flamingoat\n')
 
-    ref = input('Referral code: ')
-    threads = int(input('Threads: '))
-    ref_list = [ref] * threads
+    REF_CODE = input('Referral code: ')
+    THREADS = int(input('Threads: '))
 
-    with ThreadPoolExecutor(max_workers=threads) as executor:
-        executor.map(main, ref_list)
+    with ThreadPoolExecutor(max_workers=THREADS) as executor:
+        executor.submit(main)
